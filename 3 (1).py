87b03e15500a3bc64840b7168d9533aabc1d0d66
lenok-ar <leno4ka279a@gmail.com>
2024-12-06 18:01:08 +0700

    Add files via upload

---
+++ 3 (1).py
@@ -0,0 +1,19 @@
+import telebot
+a = telebot.TeleBot("7477245036:AAGpbRftyqGEAqhQ6B9Po7yWPhKDid5uwDM")
+
+user_data = {}
+
+@a.message_handler(commands=['start'])
+def startWork(message):
+    user_data[message.chat.id] = {}
+    keyboard_test = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard_close = telebot.types.KeyboardButton("Закрыть")
+    keyboard_test.add(keyboard_close)
+    a.send_message(message.chat.id, "Привет. Чтобы закрыть клавиатуру, тебе нужно закрыть кнопку", reply_markup=keyboard_test)
+
+@a.message_handler(content_types=['text'])
+def sendYourMessage(message):
+    if message.text == "Закрыть":
+        a.send_message(message.chat.id, "Клавиатура закрыта.", reply_markup=telebot.types.ReplyKeyboardRemove())
+
+a.polling()
